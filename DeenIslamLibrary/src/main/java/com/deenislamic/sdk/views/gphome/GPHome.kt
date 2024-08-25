d0c63887e8753ba5127290d19f358b1a1a4ff644
Rabiul Hasan <rabiul.gakk@gmail.com>
2024-08-25 17:26:53 +0600

    - Subscription related issue fixed - Feature wise tracking updated - Monthly prayer tracker issue fixed - new version 0.0.11

--- DeenIslamLibrary/src/main/java/com/deenislamic/sdk/views/gphome/GPHome.kt
+++ DeenIslamLibrary/src/main/java/com/deenislamic/sdk/views/gphome/GPHome.kt
@@ -740,8 +740,8 @@ class GPHome @JvmOverloads constructor(
     fun init(context: Context,token:String,language:String, callback: DeenSDKCallback){
         DeenSDKCore.setDeenLanguage(language)
         DeenSDKCore.setGPHomeViewInternalCallback(this)
-        //DeenSDKCore.initDeen(context,token,callback)
-        DeenSDKCore.authSDK(context,token,callback)
+        DeenSDKCore.initDeen(context,token,callback)
+        //DeenSDKCore.authSDK(context,token,callback)
     }
 
     fun changeLanguage(language:String){
